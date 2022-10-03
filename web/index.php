e7fe863413194cbf49f6e4d961ef77c9f7cf4f9b
franciscoalecrim77 <francisco_crim@hotmail.com>
2022-10-03 11:55:26 -0300

    teste

---
+++ web/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Convite</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <style>@import url('https://fonts.googleapis.com/css2?family=Alice&display=swap');</style>
+
+<style>
+.confirmacao {
+    font-size: 15px;
+    font-family: Arial;
+    
+    height: 50px;
+    border-width: 1px;
+    color: #fff;
+    border-color: #18ab29;
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+    border-bottom-left-radius: 12px;
+    border-bottom-right-radius: 12px;
+    text-shadow: 1px 1px 0px #2f6627;
+    background: blue;
+    
+}
+.confirmacaoLista {
+    font-size: 15px;
+    font-family: Arial;
+    height: 50px;
+    width: 160px;
+    border-width: 1px;
+    color: #fff;
+    border-color: #18ab29;
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+    border-bottom-left-radius: 12px;
+    border-bottom-right-radius: 12px;
+    text-shadow: 1px 1px 0px #2f6627;
+    background: blue;
+    
+}
+
+.confirmacao:hover {
+  background: #2F80ED;
+}
+
+.confirmacaoLista:hover {
+  background: #2F80ED;
+}
+</style>
+</head>
+<body>
+
+        <div  class="titulo">
+        <legend> Convite para comemorar o nosso casamento!</legend>
+    </div>
+
+    <p class="mensagem">"Quis o destino que nos encontrássemos, quis a vida que nos amássemos e queremos, com amor, construir nosso destino..." </p>
+    <div class="foto">
+        <img class="foto" src="img/casal.jpg" >
+    </div>
+
+    <div class="mensagem">
+        
+       
+     <p >Gostaríamos de compartilhar este momento tão importante para nós com você!
+        Iremos comemorar no dia 17/05/2022, as 19:30hrs no restaurante <a href="https://www.celeirodafazenda.com.br/">Celeiro da Fazenda!</a>
+    </p>
+    
+    
+    <p></p>
+</div>
+<div class="button">
+
+    <a href="cadastro.php" method="post"> 
+        <input type="submit" value="Quero confirmar presença!" class="confirmacao">
+    </a>
+</div>
+
+    <div class="mensagem"> <p>Abaixo está nossas listas de presentes! Mas caso não possa comparecer e queira nos ajudar a construir nosso sonho, entre e fique a vontade para nos presentear com o que for possível, agradecemos imensamente toda e qualquer ajuda.</p>
+</div>
+
+    <div class="lista">
+
+        <a href="https://www.precolandia.com.br/lista-casamento/li-152512" target="_blank"> 
+            <input type="submit" value="Lista PreçoLandia" class="confirmacaoLista">
+        </a>
+        <br/>
+        <a href="HTTPS://LISTAS.CASASBAHIA.COM.BR/FRANCISCOECAROLINA" target="_blank"> 
+            <input type="submit" value="Lista Casas Bahiateste" class="confirmacaoLista">
+        </a>
+
+        
+    </div>
+
+    
+
+        
+
+    
+    
+</body> 
+</html>
+
+<?php
+
